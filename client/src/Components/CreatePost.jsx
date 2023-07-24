c9e1da348e99f12043156fd931ce2d803c8676d7
farmantasleem <iamfarmanmalik@gmail.com>
2023-07-24 20:03:15 +0530

    add post modal built

--- client/src/Components/CreatePost.jsx
+++ client/src/Components/CreatePost.jsx
@@ -1,6 +1,7 @@
 import { Modal,ModalOverlay,Button,useDisclosure, ModalContent,ModalHeader,ModalFooter,ModalBody,ModalCloseButton,
   } from '@chakra-ui/react'
 import { CgAddR } from 'react-icons/cg'
+import { UploadDrawer } from './UploadDrawer'
 export const CreatePost=()=>{
 
         const { isOpen, onOpen, onClose } = useDisclosure()
@@ -13,9 +14,7 @@ export const CreatePost=()=>{
               <ModalContent maxWidth={{base:"80%",md:"400px"}} justifySelf={"center"} alignSelf={"center"}>
                 <ModalCloseButton />
                 <ModalBody maxWidth={{base:"80vw",md:"400px"}} gap="10px" marginBottom={"40px"} marginTop={"40px"}>
-                    <Button leftIcon={<CgAddR/>} minW={"100%"} maxW={"100%"}>
-                            Create a post
-                    </Button>
+                   <UploadDrawer/>
                     <Button leftIcon={<CgAddR/>} minW={"full"}>  
                         Create a story
                          </Button>
---
+++ client/src/Components/UploadDrawer.jsx
@@ -0,0 +1,58 @@
+import {
+    Drawer,
+    DrawerBody,
+    DrawerFooter,
+    DrawerHeader,
+    DrawerOverlay,
+    DrawerContent,
+    DrawerCloseButton,
+    Button,
+    Input,useDisclosure, FormLabel
+  } from '@chakra-ui/react'
+
+  import React from 'react'
+import { AiOutlineArrowLeft } from 'react-icons/ai'
+import { CgAddR } from 'react-icons/cg'
+import { FaCamera, FaImages } from 'react-icons/fa'
+import { NavLink } from 'react-router-dom'
+ export  function UploadDrawer() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const btnRef = React.useRef()
+  
+    return (
+      <>
+      
+        <Button leftIcon={<CgAddR/>} ref={btnRef} onClick={onOpen} minW={"100%"} maxW={"100%"}>
+                            Create a post
+                    </Button>
+        <Drawer
+          isOpen={isOpen}
+          placement='right'
+          onClose={onClose}
+          finalFocusRef={btnRef}
+          size={"full"}
+          padding="0px"
+          margin="0px"
+        >
+     
+          <DrawerContent  minH={"100vh"} margin={"0px"} padding={"0px"}>
+       
+            <NavLink onClick={onClose} style={{padding:"20px",backgroundColor:"rgb(248,248,248)"}}> <AiOutlineArrowLeft fontSize={"1.3rem"} color="black"/></NavLink>
+
+
+            <DrawerBody padding={"40px"} paddingTop={"0px"} margin={"0px"} backgroundColor={"rgb(248,248,248)"} >
+                <FormLabel fontSize={"1.3rem"} paddingBottom={"10px"} paddingLeft={"20px"} for="img" display={"flex"} alignItems={"center"} gap={"10px"}   w={"full"} borderBottom={"2px"} borderBottomColor={"rgb(236,236,236)"} backgroundColor={"transparent"}>
+                <input type="file"  name="uploadfile" id="img" style={{display:"none"}}/>
+                <FaImages/>
+                <label for="img" >Pick from gallery</label>
+
+                </FormLabel>
+                <Button w="full" fontSize={"1.3rem"} justifyItems={"left"} justifyContent={"left"} leftIcon={<FaCamera/>} backgroundColor={"transparent"}>Capture with Camera</Button>
+            </DrawerBody>
+  
+        
+          </DrawerContent>
+        </Drawer>
+      </>
+    )
+  }
